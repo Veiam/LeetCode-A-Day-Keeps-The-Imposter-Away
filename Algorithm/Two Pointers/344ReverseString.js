10ed4a3ec144982e050c8f74c5ccb3d54eac1d9c
Day Yang <skyipie@gmail.com>
2021-10-15 17:52:57 -0700

    Day 4 Two Pointers

---
+++ Algorithm/Two Pointers/344ReverseString.js
@@ -0,0 +1,28 @@
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// Example 1:
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+// Example 2:
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+// Constraints:
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+const reverseString = function (s) {
+    let start = 0, end = s.length - 1;
+    // until we reach the middle
+    while (start < end) {
+        // swap start and end indexes
+        [s[start], s[end]] = [s[end], s[start]];
+        start++;
+        end--;
+    }
+};
---
+++ Algorithm/Two Pointers/557ReverseWordsInAString.js
@@ -0,0 +1,61 @@
+// Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+// Example 1:
+// Input: s = "Let's take LeetCode contest"
+// Output: "s'teL ekat edoCteeL tsetnoc"
+
+// Example 2:
+// Input: s = "God Ding"
+// Output: "doG gniD"
+
+// Constraints:
+// 1 <= s.length <= 5 * 104
+// s contains printable ASCII characters.
+// s does not contain any leading or trailing spaces.
+// There is at least one word in s.
+// All the words in s are separated by a single space.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseWords = function (s) {
+    // turn s into an array
+    s = s.split('');
+    let = start = 0;
+    // loop through the array
+    for (let cur = 0; cur <= s.length; cur++) {
+        // if we reached the end or current index holds a space
+        if (s[cur] === ' ' || cur === s.length) {
+            // set an end value to current index - 1
+            let end = cur - 1;
+            // until we reach the middle
+            while (start < end) {
+                // swap start and end
+                [s[start], s[end]] = [s[end], s[start]];
+                start++;
+                end--;
+            }
+            // set the next start to a character after the current space
+            start = cur + 1;
+        }
+    }
+    // join the array back as a string.
+    return s.join('');
+};
+
+//one liner vs two pointers above
+const reverseWords = function (s) {
+    // split string into an array with a space as a separator
+    return s.split(' ').
+        // for each index
+        map(w =>
+            // split a string into an array
+            w.split('').
+                // reverse an array
+                reverse().
+                // join it back as a string
+                join('')).
+        // join array into a string with a space as a separator
+        join(' ');
+};
