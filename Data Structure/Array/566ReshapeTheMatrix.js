948ac51e84c1105052ece7c3528a7411d095863c
Veiam <skyipie@gmail.com>
2022-03-30 21:38:49 -0400

    D5. Array

--- Data Structure/Array/566ReshapeTheMatrix.js
+++ Data Structure/Array/566ReshapeTheMatrix.js
@@ -1,4 +1,4 @@
-// In MATLAB, there is a handy function called reshape which can reshape an m x n matrix
+//In MATLAB, there is a handy function called reshape which can reshape an m x n matrix
 // into a new one with a different size r x c keeping its original data.
 // You are given an m x n matrix mat and two integers r and c representing the number of rows
 // and the number of columns of the wanted reshaped matrix.
