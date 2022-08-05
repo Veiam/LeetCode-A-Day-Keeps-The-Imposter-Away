ca534a2c18c6b0d34b016e605d84814231d5a8c3
Veiam <skyipie@gmail.com>
2022-08-04 22:02:17 -0400

    Blind Day 18

---
+++ Blind/Backtracking/79WordSearch.js
@@ -0,0 +1,65 @@
+// Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+// The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+// Example 1:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+// Output: true
+
+// Example 2:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+// Output: true
+
+// Example 3:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+// Output: false
+
+// Constraints:
+// m == board.length
+// n = board[i].length
+// 1 <= m, n <= 6
+// 1 <= word.length <= 15
+// board and word consists of only lowercase and uppercase English letters.
+
+// Follow up: Could you use search pruning to make your solution faster with a larger board?
+
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ * Time complexity: O(N * 3 ^ L), n is number of cells and l is the length of word
+ * Space complexity: O(L), we need to recursion up to length of wor
+ */
+var exist = function (board, word) {
+    const rows = board.length;
+    const cols = board[0].length;
+    const dirs = [[1, 0], [-1, 0], [0, -1], [0, 1]];
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (backtrack(r, c, word))
+                return true;
+        }
+    }
+    return false;
+    function backtrack(row, col, remain) {
+        if (remain === '') {
+            return true;
+        }
+        if (row < 0 || col < 0 || row == rows || col == cols) {
+            return false;
+        }
+        const cell = board[row][col];
+        if (cell !== remain[0]) {
+            return false;
+        }
+        board[row][col] = ' '
+
+        for (const [x, y] of dirs) {
+            if (backtrack(row + x, col + y, remain.slice(1))) {
+                return true;
+            }
+        }
+        board[row][col] = cell;
+    }
+
+};
